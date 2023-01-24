c1424d50308bf5eabf48858a952788314d3d1947
Sabal Shrestha <sabalshrestha427@gmail.com>
2023-01-24 17:35:12 +0545

    Services Added

--- header.js
+++ header.js
@@ -20,7 +20,18 @@ class MyHeader extends HTMLElement {
             <div class="navbar-nav mx-auto">
               <a class="nav-link" href="index.html">Home</a>
               <a class="nav-link" href="about-us.html">About Us</a>
-              <a class="nav-link" href="services.html">Services</a>
+              <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Services
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="services.html">All Services</a></li>
+                <li><a class="dropdown-item" href="hardware-consultation-solution.html">Hardware Consultation and Solution</a></li>
+                <li><a class="dropdown-item" href="networking-security-surveillance-solution.html">Networking and Security Surveillance Solution</a></li>
+                <li><a class="dropdown-item" href="website-design.html">Website Design</a></li>
+                <li><a class="dropdown-item" href="logo-graphics-design.html">Logo Design and Graphics Design</a></li>
+              </ul>
+            </li>
               <a class="nav-link" href="blogs.html">Blogs</a>
               <a class="nav-link" href="#">Contact</a>
             </div>
